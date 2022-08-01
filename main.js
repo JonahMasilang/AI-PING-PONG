d11e1fa781e486c7d7160eb47093e5c353be4899
JonahMasilang <89702146+JonahMasilang@users.noreply.github.com>
2022-08-01 09:13:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,20 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  image(video, 0, 0, 700, 600);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
+function modelLoaded() {
+  console.log('Model loaded.');
+}
 
 function draw(){
 
